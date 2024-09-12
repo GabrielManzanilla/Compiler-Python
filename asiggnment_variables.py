7a411f23cac0d771f33b250b9e85999f1d05568c
GabrielManzanilla <gabriel29444@gmail.com>
2024-09-11 20:47:19 -0600

    fix(asggments_variables.py) refactor the function define_vars() to identify if is assiggment variable or it's operation

--- asiggnment_variables.py
+++ asiggnment_variables.py
@@ -6,28 +6,27 @@ regex=r"^_[A-Z][a-zA-Z0-9_]*$"
 
 def assignment_var_details(target, node):
     if re.match(regex, target.id): #comprueba sintaxis de la variable con regex
-        value_data=(node.body[0].value.value)
+        value_data=(node.value)
         type_data= type(value_data) #llama a una funcion para que se asigne si es de tipo real-int o string-char
-        variable=str(target.id)
+        variable=target.id
         return variable,type_data,value_data #se añade al diccionario la variable, su tipo de dato y el dato que la variable contiene
     else:
         print("sintaxis de variable no aceptada", target.id)
         return None, None, None
 
 
-
 def define_vars(code_line):
-    try:
-        node =ast.parse(code_line, mode='exec')
-        if isinstance(node.body[0], ast.Assign): #compruba que la linea sea de asignacion
-            for target in node.body[0].targets:
-                return assignment_var_details(target, node)
-        elif isinstance(node.body[0], ast.Assign):
-            operation=node.body[0].value
-            
-                # if vars[target.id]:  return assignment_var_details(target, node)
+        tree =ast.parse(code_line)
+        for node in tree.body:
+            if isinstance(node, ast.Assign): #compruba que la linea sea de asignacion
 
-    except Exception as e:
-        print(f"Error in {e}")
+                if isinstance(node.value, ast.BinOp):
+                        left=node.value.left.id if isinstance(node.value.left, ast.Name) else None
+                        print(left)
+                        right=node.value.right.id  if isinstance(node.value.right, ast.Name) else None
+                        print(right)
+                        return None, None, None
+                elif isinstance(node.value, ast.Constant):
+                        return assignment_var_details(node.targets[0], node.value)
+
         return None, None, None
-
