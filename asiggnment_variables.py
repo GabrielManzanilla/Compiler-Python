b8d274ff8cd564a91393e7a8a1a1dc466236d937
GabrielManzanilla <gabriel29444@gmail.com>
2024-09-14 14:10:28 -0600

    feat (asiggnment_variables) Add funtionality to return the value before the operationin the module parse_bin

--- asiggnment_variables.py
+++ asiggnment_variables.py
@@ -1,6 +1,7 @@
 #--importacion de modulos--
 import ast
 import re
+import operator
 #--importacion de funciones externas
 from breaker import break_error_regex
 from compare import compare_types
@@ -10,6 +11,20 @@ import config
 
 
 regex=r"^_[A-Z][a-zA-Z0-9_]*$"
+
+def obtener_operador(binop):
+    operadores = {
+        ast.Add: operator.add,      # Suma para números y concatenación para cadenas
+        ast.Sub: operator.sub,      # Resta
+        ast.Mult: operator.mul,     # Multiplicación
+        ast.Div: operator.truediv,  # División
+        ast.FloorDiv: operator.floordiv,  # División entera
+        ast.Mod: operator.mod,      # Módulo
+        ast.Pow: operator.pow       # Potencia
+    }
+
+    return operadores[type(binop)]  # Devuelve la operación correspondiente
+
 
 #----Funcion para descomponer la operacion binaria----
 def parse_binop(node, index):
@@ -23,7 +38,7 @@ def parse_binop(node, index):
         var_id=node.left.id
         if re.match(regex, var_id):
             if  var_id in config.lexemas:
-                type_data, value_data=config.lexemas[var_id]
+                type_data, left_value_data=config.lexemas[var_id]
                 left_operands.append([var_id, type_data])
              
             else: append_error(var_id, index, "Variable indefinida")
@@ -32,8 +47,8 @@ def parse_binop(node, index):
         #si hace match con el regex y ya se encuentra en la tabla de lexemas se añade el id y su tipo a la lista, sino se manda a una funcion para insertar errores
         #type_data, value_data=config.lexemas[var_id]
     elif isinstance(node.left, ast.Constant):
-        dato=node.left.value
-        left_operands.append([dato, type(dato)])  # Si es una constante se añade al arreglo el dato y su tipo
+        left_value_data=node.left.value
+        left_operands.append([left_value_data, type(left_value_data)])  # Si es una constante se añade al arreglo el dato y su tipo
 
     # Lado derecho
     if isinstance(node.right, ast.BinOp):
@@ -42,7 +57,7 @@ def parse_binop(node, index):
         var_id=node.right.id
         if re.match(regex, var_id):
             if  var_id in config.lexemas:
-                type_data, value_data=config.lexemas[var_id]
+                type_data, right_value_data=config.lexemas[var_id]
                 right_operands.append([var_id, type_data])
              
             else: append_error(var_id, index, "Variable indefinida")
@@ -50,11 +65,15 @@ def parse_binop(node, index):
         else: break_error_regex(var_id)
 
     elif isinstance(node.right, ast.Constant):
-        dato=node.right.value
-        right_operands.append([dato, type(dato)]) # Si es una constante, extraer el valor
+        right_value_data=node.right.value
+        right_operands.append([right_value_data, type(right_value_data)]) # Si es una constante, extraer el valor
 
-    
-    return left_operands + right_operands
+    operator=obtener_operador(node.op)
+    try:
+        value_result=operator(left_value_data, right_value_data)
+    except:
+        value_result=None
+    return left_operands + right_operands, value_result
 
 
 
@@ -67,9 +86,9 @@ def identify_operation(code_line, index):
                 var_id=node.targets[0].id #se obtiene el nombre de la variable
                 if isinstance(node.value, ast.BinOp):
                     if re.match(regex, var_id): #se comprueba que la variable tenga la sintaxis correcta
-                        operands= parse_binop(node.value, index)
+                        operands, value_result= parse_binop(node.value, index)
                         type_result=compare_types(operands, index)
-                        config.lexemas[var_id]=(type_result, None)
+                        config.lexemas[var_id]=(type_result, value_result)
 
                     else:
                         break_error_regex(var_id)
